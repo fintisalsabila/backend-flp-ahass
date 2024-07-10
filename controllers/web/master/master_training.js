682a3d675663ae609bfdc86f604997a7c20795b6
fintisalsabila <fintisalsabila@gmail.com>
2024-07-10 16:29:41 +0700

    1. idmodul nya masih berantakan ga auto increment. 2. blom benerin bagian view master_modul itu error, selebih itu add edit dan delete nya aman

--- controllers/web/master/master_training.js
+++ controllers/web/master/master_training.js
@@ -4,7 +4,43 @@ var uuidv1 = require('uuid/v1');
 
 function MasterTraining() {
     this.addForm = function(req, res) {
-        res.render('master_training/add');
+        connection.acquire(function(err, con) {
+            if (err) throw err;
+            con.query('SELECT IdModul, modulName FROM master_modul', function(err, results) {
+                con.release();
+                if (err) {
+                    console.log(err);
+                    res.render('master_training/add', { modules: [] });
+                } else {
+                    res.render('master_training/add', { modules: results });
+                }
+            });
+        });
+    };
+
+    // Ensure the `editForm` method also fetches the `modulName`
+    this.editForm = function(req, res) {
+        var id = req.query.id;
+        connection.acquire(function(err, con) {
+            if (err) throw err;
+            con.query('SELECT * FROM master_training WHERE IdTraining = ?', [id], function(err, trainingResults) {
+                if (err) {
+                    con.release();
+                    console.log(err);
+                    res.render('master_training/edit', { data: {}, modules: [] });
+                } else {
+                    con.query('SELECT IdModul, modulName FROM master_modul', function(err, moduleResults) {
+                        con.release();
+                        if (err) {
+                            console.log(err);
+                            res.render('master_training/edit', { data: trainingResults[0], modules: [] });
+                        } else {
+                            res.render('master_training/edit', { data: trainingResults[0], modules: moduleResults });
+                        }
+                    });
+                }
+            });
+        });
     };
 
     this.submitInsertMasterTrain = function(req, res) {
@@ -13,11 +49,32 @@ function MasterTraining() {
         var participant = req.body.participant;
         var trainingStartDate = req.body.trainingStartDate;
         var trainingEndDate = req.body.trainingEndDate;
-        var modul = req.body.modul;
+        var modulName = req.body.modulName;
 
         connection.acquire(function(err, con) {
             if (err) throw err;
-            con.query('INSERT INTO master_training SET ?', { IdTraining: id, trainingName, participant, trainingStartDate, trainingEndDate, modul }, function(err, results) {
+            con.query('INSERT INTO master_training SET ?', { IdTraining: id, trainingName, participant, trainingStartDate, trainingEndDate, modulName }, function(err, results) {
+                con.release();
+                if (err) {
+                    console.log(err);
+                } else {
+                    res.redirect('/MasterTraining/Index');
+                }
+            });
+        });
+    };
+
+    this.submitUpdateMasterTrain = function(req, res) {
+        var id = req.params.id;
+        var trainingName = req.body.trainingName;
+        var participant = req.body.participant;
+        var trainingStartDate = req.body.trainingStartDate;
+        var trainingEndDate = req.body.trainingEndDate;
+        var modulName = req.body.modulName;
+
+        connection.acquire(function(err, con) {
+            if (err) throw err;
+            con.query('UPDATE master_training SET ? WHERE IdTraining = ?', [{ trainingName, participant, trainingStartDate, trainingEndDate, modulName }, id], function(err, results) {
                 con.release();
                 if (err) {
                     console.log(err);
@@ -46,44 +103,44 @@ function MasterTraining() {
         });
     };
 
-    this.editForm = function(req, res) {
-        var id = req.query.id;
-        connection.acquire(function(err, con) {
-            if (err) throw err;
-            con.query('SELECT * FROM master_training WHERE IdTraining = ?', [id], function(err, results) {
-                con.release();
-                if (err) {
-                    console.log(err);
-                } else {
-                    res.render('master_training/edit', {
-                        title: 'Edit Data Master Training',
-                        data: results[0]
-                    });
-                }
-            });
-        });
-    };
+    // this.editForm = function(req, res) {
+    //     var id = req.query.id;
+    //     connection.acquire(function(err, con) {
+    //         if (err) throw err;
+    //         con.query('SELECT * FROM master_training WHERE IdTraining = ?', [id], function(err, results) {
+    //             con.release();
+    //             if (err) {
+    //                 console.log(err);
+    //             } else {
+    //                 res.render('master_training/edit', {
+    //                     title: 'Edit Data Master Training',
+    //                     data: results[0]
+    //                 });
+    //             }
+    //         });
+    //     });
+    // };
 
-    this.submitUpdateMasterTrain = function(req, res) {
-        var id = req.params.id;
-        var trainingName = req.body.trainingName;
-        var participant = req.body.participant;
-        var trainingStartDate = req.body.trainingStartDate;
-        var trainingEndDate = req.body.trainingEndDate;
-        var modul = req.body.modul;
+    // this.submitUpdateMasterTrain = function(req, res) {
+    //     var id = req.params.id;
+    //     var trainingName = req.body.trainingName;
+    //     var participant = req.body.participant;
+    //     var trainingStartDate = req.body.trainingStartDate;
+    //     var trainingEndDate = req.body.trainingEndDate;
+    //     var modulName = req.body.modulName;
 
-        connection.acquire(function(err, con) {
-            if (err) throw err;
-            con.query('UPDATE master_training SET ? WHERE IdTraining = ?', [{ trainingName, participant, trainingStartDate, trainingEndDate, modul }, id], function(err, results) {
-                con.release();
-                if (err) {
-                    console.log(err);
-                } else {
-                    res.redirect('/MasterTraining/Index');
-                }
-            });
-        });
-    };
+    //     connection.acquire(function(err, con) {
+    //         if (err) throw err;
+    //         con.query('UPDATE master_training SET ? WHERE IdTraining = ?', [{ trainingName, participant, trainingStartDate, trainingEndDate, modulName }, id], function(err, results) {
+    //             con.release();
+    //             if (err) {
+    //                 console.log(err);
+    //             } else {
+    //                 res.redirect('/MasterTraining/Index');
+    //             }
+    //         });
+    //     });
+    // };
 
     this.deleteMasterTrain = function(req, res) {
         var id = req.query.id;
