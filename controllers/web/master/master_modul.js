b3c72c9053f2ff1e3d6d0b7b4aaffa7489c9884c
fintisalsabila <fintisalsabila@gmail.com>
2024-07-19 15:24:49 +0700

    berhasil edit modul (logicnya hapus fileUpload ketika klik button edit pada index dan pada edit akan terinput data baru)

--- controllers/web/master/master_modul.js
+++ controllers/web/master/master_modul.js
@@ -99,27 +99,60 @@ function MasterModul() {
     this.submitUpdateModul = function(req, res) {
         var IdModul = req.params.id;
         var modulName = req.body.modulName;
-        var fileUpload = req.file ? req.file.filename : req.body.oldFile;
-
+        var fileUpload = req.files[0] ? req.files[0].filename : null;
+        var modifiedAt = new Date();
+        var modifiedBy = req.session.user.id;
+    
+        if (!fileUpload) {
+            return res.status(400).send('File upload is required.');
+        }
+    
         connection.acquire(function(err, con) {
             if (err) {
                 console.log(err);
                 return res.redirect('/MasterModul/Edit?id=' + IdModul);
             }
-            var query = 'UPDATE master_modul SET modulName = ?, fileUpload = ?, modifiedAt = ?, modifiedBy = ? WHERE IdModul = ?';
-            var params = [modulName, fileUpload, new Date(), req.session.user.id, IdModul];
-
-            con.query(query, params, function(err, results) {
-                con.release();
+    
+            // Get the old file name for deletion
+            con.query('SELECT fileUpload FROM master_modul WHERE IdModul = ?', [IdModul], function(err, results) {
                 if (err) {
+                    con.release();
                     console.log(err);
                     return res.redirect('/MasterModul/Edit?id=' + IdModul);
-                } else {
-                    res.redirect('/MasterModul/Index');
                 }
+    
+                var oldFile = results[0].fileUpload;
+    
+                // Update the record with the new fileUpload
+                var query = 'UPDATE master_modul SET modulName = ?, fileUpload = ?, modifiedAt = ?, modifiedBy = ? WHERE IdModul = ?';
+                var params = [modulName, fileUpload, modifiedAt, modifiedBy, IdModul];
+    
+                con.query(query, params, function(err, results) {
+                    con.release();
+                    if (err) {
+                        console.log(err);
+                        return res.redirect('/MasterModul/Edit?id=' + IdModul);
+                    } else {
+                        // Delete the old file
+                        var fs = require('fs');
+                        var path = require('path');
+                        var oldFilePath = path.join(__dirname, '../../../uploads', oldFile);
+    
+                        fs.unlink(oldFilePath, function(err) {
+                            if (err) {
+                                console.log('Failed to delete old file:', oldFile);
+                            } else {
+                                console.log('Successfully deleted old file:', oldFile);
+                            }
+                        });
+    
+                        res.redirect('/MasterModul/Index');
+                    }
+                });
             });
         });
     };
+    
 
     this.deleteModul = function(req, res) {
         var IdModul = req.query.id;
